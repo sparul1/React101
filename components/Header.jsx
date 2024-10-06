cb43c478963d33e8275a34e6f581bebc7b423a52
sparul1 <parul82811@gmail.com>
2024-10-06 17:26:31 +0530

    added logo img

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,8 @@
 export default Header = ()=>{
     return (
         <header className = "header">
-            <h1 className="headerTitle">GAME OF THRONES </h1>
-            <h2 className="headerSubtitle">Characters Digest</h2>
+            <img className="logo-image" src="https://seeklogo.com/images/G/game-of-thrones-logo-3A574D3ECB-seeklogo.com.png" alt="" />
+            {/* <h2 className="headerSubtitle">Characters Digest</h2> */}
         </header>
     )
 }
