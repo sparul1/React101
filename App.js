de61acf9c4e714794c9251e078f2374cc116ab36
sparul1 <parul82811@gmail.com>
2024-10-06 14:38:11 +0530

    React app basic

---
+++ App.js
@@ -0,0 +1,16 @@
+// App.js
+import React from 'react';
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <h1>Welcome to My React App</h1>
+        <p>This is a simple app structure.</p>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
